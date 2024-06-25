0d2229f280e485d6451868823bc30f00d4f99302
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-06-25 19:05:58 +0300

    Added AllPosts component with server-side prerendering and data fetching.

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,16 @@
-export default function AllPostsPage(){
-    return <h1>Posts Page</h1>
+import AllPosts from "../../components/posts/all.posts.js";
+import { getAllPosts } from "../../lib/posts-util.js";
+
+export default function AllPostsPage({ posts }) {
+    return <AllPosts posts={posts} />
+}
+
+export async function getStaticProps() {
+    const allPosts = getAllPosts();
+
+    return {
+        props: {
+            posts: allPosts
+        }
+    };
 }
