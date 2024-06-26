b38992c4688f0e9846ac189d0ac4716f4d3e2718
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-06-26 17:04:40 +0300

    Added another .md file and used the 'react-syntax-highlighter' library to better visualize code snippets from Markdown.

--- components/posts/post-detail/detail-content.js
+++ components/posts/post-detail/detail-content.js
@@ -1,4 +1,7 @@
 import Markdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
 import Image from 'next/image';
 
 import DetailHeader from './detail-header.js';
@@ -39,8 +42,19 @@ export default function DetailContent({ post }) {
 
             return <p>{paragraph.children}</p>;
         },
-      
-    }
+        code(code) {
+            const { className, children } = code;
+            const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+            return (
+                <SyntaxHighlighter
+                    style={dracula}
+                    language={language}
+                    children={children}
+                />
+            );
+        }
+    };
+
 
     return (
         <article className={classes.content} >
