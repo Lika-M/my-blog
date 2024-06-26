45eb3afcef7b9b03a29e42e9e925a771628ca756
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-06-26 13:56:32 +0300

    Implemented rendering images from Markdown using the Next.js Image component.

--- components/posts/post-detail/detail-content.js
+++ components/posts/post-detail/detail-content.js
@@ -1,18 +1,53 @@
-import ReactMarkdown from 'react-markdown';
+import Markdown from 'react-markdown';
+import Image from 'next/image';
 
-import classes from './detail-content.module.css';
 import DetailHeader from './detail-header.js';
+import classes from './detail-content.module.css';
 
-export default function DetailContent({post}) {
-    
+export default function DetailContent({ post }) {
+
     const imgPath = `/images/posts/${post.image}`;
-    
+
+    const customRenderers = {
+        // img(image) {
+        //     return <Image
+        //         src={`/images/posts/${image}.src`}
+        //         alt={image.alt}
+        //         width={600}
+        //         height={300}
+
+        //     />
+        // },
+
+        p(paragraph) {
+            const { node } = paragraph;
+
+            if (node.children[0].tagName === 'img') {
+                const image = node.children[0];
+
+                return (
+                    <div className={classes.image}>
+                        <Image
+                            src={`/images/posts/${image.properties.src}`}
+                            alt={image.alt}
+                            width={600}
+                            height={300}
+                        />
+                    </div>
+                )
+            }
+
+            return <p>{paragraph.children}</p>;
+        },
+      
+    }
+
     return (
-        <article className={classes.content}>
+        <article className={classes.content} >
             <DetailHeader title={post.title} image={imgPath} />
-            <ReactMarkdown>
+            <Markdown components={customRenderers}>
                 {post.content}
-            </ReactMarkdown>
-        </article>
+            </Markdown>
+        </article >
     );
 }
